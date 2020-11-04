3f5a8d40bea6b33ef2e2d1318973cafaa7048856
Alexander Chaschin <chaschin@uchi.ru>
2020-11-05 01:20:06 +0300

    add time and not_found routes

--- app.rb
+++ app.rb
@@ -1,5 +1,34 @@
 class App
   def call(env)
+    path = env['REQUEST_PATH']
+    params = parse_query(env['QUERY_STRING'])
+    router(path, params)
+  end
+
+  def router(path, params)
+    case path
+    when '/time'
+      time(params)
+    else
+      not_found
+    end
+  end
+
+  def time(params)
+    ok
+  end
+
+  private
+
+  def parse_query(query)
+    query.split('&').map { |s| s.split('=') }.to_h
+  end
+
+  def ok
     [200, {}, []]
+  end
+
+  def not_found
+    [404, {}, []]
   end
 end
